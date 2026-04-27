871acc73844ef15acf3550aaf931da5c6b8d11a8
Starligth99 <miguelromeroalcantara@gmail.com>
2026-04-27 03:43:19 -0600

    Configuración final de base de datos Neon y guardián de seguridad

--- Corralon/settings.py
+++ Corralon/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 import dj_database_url
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
